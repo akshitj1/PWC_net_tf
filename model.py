4a47a6b3a7a7ad52d6a05b81bd91c567c5d83d6a
Akshit Jain <jaina488@gmail.com>
2020-10-01 10:25:25 +0530

    model compiles without eager execution

--- model.py
+++ model.py
@@ -2,60 +2,86 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 print(tf.__version__)
 
+# shared modules using keras functional API: https://github.com/google-research/google-research/blob/master/video_structure/vision.py
 
-def conv(output_channels=16, kernel_size=3, downsample=False):
+
+def conv(output_channels=16, kernel_size=3, downsample=False, name=None):
     # todo: add relu
     stride = 2 if downsample else 1
     return tf.keras.layers.Conv2D(filters=output_channels,
-                                  kernel_size=kernel_size, strides=stride, padding='same')
+                                  kernel_size=kernel_size, strides=stride, padding='same', name='{}_conv'.format(name) if name else None)
 
 
-def deconv(output_channels=2, kernel_size=4, upsample=True):
+def deconv(output_channels=2, kernel_size=4, upsample=True, name=None):
     stride = 2 if upsample else 1
     # todo: add padding. bias is true for deconv? check default in pytroch vs tf
     return tf.keras.layers.Conv2DTranspose(filters=output_channels,
-                                           kernel_size=kernel_size, strides=stride, padding='same')
+                                           kernel_size=kernel_size, strides=stride, padding='same', name='{}_deconv'.format(name))
 
 
-def downsample_conv_seq(x, output_channels=16):
-    conv_a = conv(output_channels, downsample=True)
-    conv_aa = conv(output_channels)
-    conv_b = conv(output_channels)
-    return conv_b(conv_aa(conv_a(x)))
-    # return tf.keras.Sequential([conv_a, conv_aa, conv_b])
+def build_downsample_block(output_channels=16, name=None):
+    conv_a = conv(output_channels, downsample=True, name='downsample_2')
+    conv_aa = conv(output_channels, name='upreceptive_1')
+    conv_b = conv(output_channels, name='upreceptive_2')
+    return tf.keras.Sequential([conv_a, conv_aa, conv_b], name)
 
 
-def extract_features_multiscale(img, num_levels=6):
+def build_pyramid_feature_extractor(image_shape, num_levels=6):
+    img = tf.keras.Input(shape=image_shape, name='image_input')
     output_channels = 16
     ft_lvls = [None]*num_levels
     for lvl in range(0, num_levels):
+        downsample_conv_seq = build_downsample_block(
+            output_channels, name='level_{}'.format(lvl))
         ft_lvl = downsample_conv_seq(
-            ft_lvls[lvl-1] if lvl > 0 else img, output_channels)
+            ft_lvls[lvl-1] if lvl > 0 else img)
         ft_lvls[lvl] = ft_lvl
         output_channels *= 2
-    return ft_lvls
+    return tf.keras.Model(inputs=img, outputs=ft_lvls, name='image_feature_extractor')
 
 
-def build_image_input(image_width, image_height, color_channels=3):
+def build_image_input(image_shape, name='left'):
     # todo: normalize image
-    return tf.keras.Input(shape=(image_height, image_width, color_channels),
-                          batch_size=1, dtype=tf.dtypes.uint8)
+    return tf.keras.Input(shape=image_shape, dtype=tf.dtypes.uint8, name='{}_image_input'.format(name))
 
 
-def preprocess_image(img):
-    return tf.keras.layers.experimental.preprocessing.Rescaling(1./255)(img)
+def build_preprocess_image():
+    return tf.keras.layers.experimental.preprocessing.Rescaling(1./255, name='image_rescale_unit')
 
 
 def correlation_volume(im_features_l, im_features_r_warped, max_disp_x=50):
     # input B*H*W*F, B*return H*W*D
     corr_slices = []
     for disp_x in range(max_disp_x):
-        # shift with zero padding instead of roll
+        # todo: shift with zero padding instead of roll
         im_r_shifted = tf.roll(im_features_r_warped, shift=-disp_x, axis=2)
         corr_slice = tf.reduce_mean(tf.multiply(
             im_features_l, im_r_shifted), axis=-1)
         corr_slices.append(corr_slice)
-    return tf.stack(corr_slices, axis=3)
+    return tf.stack(corr_slices, axis=3, name='correlation_volume')
+
+
+# class CorrelationVolumeToFlow(tf.keras.layers.Layer):
+#     def __init__(self, num_conv_reps=5):
+#         super(Linear, self).__init__()
+#         self.num_conv_reps = num_conv_reps
+#         res_channels = [128, 96, 64, 32, 2]
+#         assert(len(res_channels) == num_conv_reps)
+#         self.rep_conv = [None]*res_channels
+#         for i in range(num_conv_reps):
+#             self.rep_conv[i] = conv(res_channels[i], name='rep_{}'.format(i))
+#         self.contract_flow = conv(res_channels[-1],
+#                                   name='compress_flow_channels')
+
+#     def call(self, corr_vol):
+#         # reduce channels from dispartiy to 2 by aplying convolutions repeatedly
+#         conv_corr_vol = corr_vol
+#         for i in range(self.num_conv_reps):
+#             conv_corr_vol = tf.keras.layers.Concatenate(axis=3)(
+#                 [conv_corr_vol, self.rep_conv[i](conv_corr_vol)])
+#         # last layer is 2 channel, hence flow
+#         pred_flow = self.contract_flow(conv_corr_vol)
+#         return pred_flow
 
 
 def corr_to_flow(corr_vol, num_conv_reps=5):
@@ -64,10 +90,10 @@ def corr_to_flow(corr_vol, num_conv_reps=5):
     assert(len(res_channels) == num_conv_reps)
     conv_corr_vol = corr_vol
     for i in range(num_conv_reps):
-        conv_corr_vol = tf.concat(
-            [conv_corr_vol, conv(res_channels[i])(conv_corr_vol)], axis=3)
+        conv_corr_vol = tf.keras.layers.Concatenate(axis=3)(
+            [conv_corr_vol, conv(res_channels[i])(conv_corr_vol)])
     # last layer is 2 channel, hence flow
-    pred_flow = conv_corr_vol
+    pred_flow = conv(res_channels[-1])(conv_corr_vol)
     return pred_flow
 
 
@@ -78,21 +104,22 @@ def warp(im_features, flow):
 def extract_multiscale_flows(img_features, num_scale_levels):
     LEFT, RIGHT = (0, 1)
     flows = [None]*num_scale_levels
+    warp_layer = tf.keras.layers.Lambda(lambda x: warp(x[0], x[1]))
     for level in range(num_scale_levels-1, -1, -1):
         if level+1 >= num_scale_levels:
-            # flow_up = tf.zeros(tf.shape(img_features[RIGHT][level])[:-1])
             flow_up = tf.zeros(
-                tf.concat([tf.shape(img_features[RIGHT][level])[:-1], tf.constant([2], dtype=tf.int32)], axis=0))
+                tf.concat([tf.shape(img_features[RIGHT][level])[:-1], tf.constant([2], dtype=tf.int32)], axis=0), name='no_flow')
         else:
-            flow_up = deconv()(flows[level+1])
+            flow_up = deconv(name='flow_upsample_{}'.format(
+                level+1))(flows[level+1])
         # todo: flow is scaled originally?
-        img_r_warped = warp(img_features[RIGHT][level], flow_up)
+        img_r_warped = warp_layer(inputs=[img_features[RIGHT][level], flow_up])
         img_l_features = img_features[LEFT][level]
         corr_vol = correlation_volume(img_l_features, img_r_warped)
         # todo: upsampled features convolved to 2 channels also concatenated in original paper
         # left features are concatenated to create U-Net like architecture
-        corr_vol_enriched = tf.concat(
-            [corr_vol, img_l_features, flow_up], axis=3)
+        corr_vol_enriched = tf.keras.layers.Concatenate(axis=3)(
+            [corr_vol, img_l_features, flow_up])
 
         flow = corr_to_flow(corr_vol_enriched)
         flows[level] = flow
@@ -114,27 +141,44 @@ def pyramid_l1_loss(flows_pred, flow_gt):
 
 
 def build_model():
+    views = [{'id': 0, 'name': 'left'}, {'id': 1, 'name': 'right'}]
     LEFT, RIGHT = (0, 1)
     num_feature_scale_levels = 6
     im_width, im_height = (1024, 1024)
-    imgs = [build_image_input(im_width, im_height)
-            for img_idx in [LEFT, RIGHT]]
-    imgs_normed = list(map(preprocess_image, imgs))
-    imgs_features = [extract_features_multiscale(
-        im, num_feature_scale_levels) for im in imgs_normed]
-    flows = extract_multiscale_flows(imgs_features, num_feature_scale_levels)
+    color_channels = 3
+    img_shape = (im_height, im_width, color_channels)
+
+    preprocess_image = build_preprocess_image()
+    extract_features_multiscale = build_pyramid_feature_extractor(
+        img_shape, num_feature_scale_levels)
+
+    imgs = []
+    ims_ft_pyramid = []
+
+    for view in views:
+        im = build_image_input(img_shape, name=view['name'])
+        imgs.append(im)
+        im_normed = preprocess_image(im)
+        im_ft_pyramid = extract_features_multiscale(im_normed)
+        ims_ft_pyramid.append(im_ft_pyramid)
+
+    flow_pyramid = extract_multiscale_flows(
+        ims_ft_pyramid, num_feature_scale_levels)
     # build loss from these flows
-    model = tf.keras.Model(inputs=imgs, outputs=flows)
+    model = tf.keras.Model(inputs=imgs, outputs=flow_pyramid[-1])
+    # tf.keras.utils.plot_model(model, "pwc_net_diagram.png", show_shapes=True)
+    print(model.summary())
+
+    # todo: replace with pyramid loss
     model.compile(optimizer=tf.keras.optimizers.RMSprop(
-        0.001), loss=pyramid_l1_loss)
+        0.001), loss='mae')
+
     return model
 
 
 def train():
-    tf.compat.v1.disable_eager_execution()
     # tf.config.run_functions_eagerly(True)
     model = build_model()
-    print(model.summary())
 
 
 if __name__ == "__main__":
--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ python3 -m venv env
 source env/bin/activate
 pip install tensorflow
 pip install tensorflow-addons
+pip install pydot
