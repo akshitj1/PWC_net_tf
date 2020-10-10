aa9e871af9d52e0089a9f7110a9f7aabc7fd1f7b
Akshit Jain <jaina488@gmail.com>
2020-10-10 15:28:37 +0530

    disparity direction fix, bilinear upsampling, flow refine with dilation, 0 crop/pad inbuilt fn.

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ def deconv(output_channels=2, kernel_size=4, upsample=True, name=None):
                                            kernel_size=kernel_size, strides=stride, padding='same', name='{}_deconv'.format(name))
 
 
-def build_downsample_block(output_channels=16, name=None):
+def downsample_block(output_channels=16, name=None):
     conv_a = conv(output_channels, downsample=True, name='downsample_2')
     conv_aa = conv(output_channels, name='upreceptive_1')
     conv_b = conv(output_channels, name='upreceptive_2')
@@ -27,21 +27,22 @@ def build_downsample_block(output_channels=16, name=None):
 
 
 def build_pyramid_feature_extractor(image_shape, num_levels):
+    # level 0 -> original resolution, level n -> l/2**n dims for l in {w,h}
+    # there are 6 levels from 2 to 7. we will not store 0,1 as they are not used
     img = tf.keras.Input(shape=image_shape, name='image_input')
-    output_channels = 16
-    ft_lvls = [None]*num_levels
-    for lvl in range(0, num_levels):
-        downsample_conv_seq = build_downsample_block(
-            output_channels, name='level_{}'.format(lvl))
-        ft_lvl = downsample_conv_seq(
-            ft_lvls[lvl-1] if lvl > 0 else img)
-        ft_lvls[lvl] = ft_lvl
-        output_channels *= 2
-    return tf.keras.Model(inputs=img, outputs=ft_lvls, name='image_feature_extractor')
+    ft_pyramid = [None]*num_levels
+    # 0th level features is the image itself. anyway we are going to use from level 2
+    ft_pyramid[0] = img
+    cur_level_op_channels = 8
+    for lvl in range(1, num_levels):
+        ft_pyramid[lvl] = downsample_block(
+            cur_level_op_channels, name='level_{}'.format(lvl))(ft_pyramid[lvl-1])
+        cur_level_op_channels *= 2
+    return tf.keras.Model(inputs=img, outputs=ft_pyramid, name='image_feature_extractor')
 
 
-def build_image_input(image_shape, name='left'):
-    return tf.keras.Input(shape=image_shape, dtype=tf.dtypes.int32, name='{}_image_input'.format(name))
+def get_image_input(image_shape, name):
+    return tf.keras.Input(shape=image_shape, dtype=tf.dtypes.int32, name=name)
 
 
 def nearest_multiple(dividend, divisor):
@@ -49,36 +50,41 @@ def nearest_multiple(dividend, divisor):
     return (q + (r > 0))*divisor
 
 
-def pad(img_shape, num_pyr_levels):
-    im_y, im_x, channels = img_shape
-    im_y_padded, im_x_padded = (nearest_multiple(
-        im_y, 2**num_pyr_levels), nearest_multiple(im_x, 2**num_pyr_levels))
-    paddings = ((0, im_y_padded-im_y), (0, im_x_padded-im_x))
-    padded_img_shape = (im_y_padded, im_x_padded, channels)
-    print('original shape: {}, padded shape: {}'.format(
-        img_shape, padded_img_shape))
-    return tf.keras.layers.ZeroPadding2D(paddings), padded_img_shape
+def pad(im, in_img_shape, num_levels):
+    divisor = 2**(num_levels-1)
+    in_h, in_w, in_c = in_img_shape
+    out_h, out_w = (nearest_multiple(in_h, divisor),
+                    nearest_multiple(in_w, divisor))
+    out_img_shape = (out_h, out_w, in_c)
+    return tf.image.resize_with_crop_or_pad(im, out_h, out_w), out_img_shape
 
 
-def crop(in_shape, padded_shape):
-    crops = np.array(padded_shape) - np.array(in_shape)
-    return tf.keras.layers.Cropping2D(((0, crops[0]), (0, crops[1])))
+def crop(im, out_shape):
+    h, w, _ = out_shape
+    return tf.image.resize_with_crop_or_pad(im, h, w)
 
 
-def build_preprocess_image(img_shape, num_pyr_levels):
-    rescale = tf.keras.layers.experimental.preprocessing.Rescaling(
-        1./255, name='image_rescale_unit')
+def build_preprocess_image(img_shape, num_levels):
+    im = get_image_input(img_shape, 'preprocess_input')
+    im_normed = tf.cast(im, dtype=tf.float32)/255.
+
     # pad to ensure dims are multiple of 2**pyr_levels
-    padding_layer, new_img_shape = pad(img_shape, num_pyr_levels)
-    return tf.keras.models.Sequential([rescale, padding_layer], name='preprocess'), new_img_shape
+    out_im, out_shape = pad(im_normed, img_shape, num_levels)
+    return tf.keras.Model(inputs=im, outputs=out_im, name='preprocessing_layer'), out_shape
 
 
-def correlation_volume(im_features_l, im_features_r_warped, max_disp_x=50):
+def correlation_volume(im_features_l, im_features_r_warped, max_disp_x=4):
     # input B*H*W*F, B*return H*W*D
     corr_slices = []
-    for disp_x in range(max_disp_x):
-        # todo: shift with zero padding instead of roll
-        im_r_shifted = tf.roll(im_features_r_warped, shift=-disp_x, axis=2)
+    im_features_r_padded = tf.keras.layers.ZeroPadding2D(
+        padding=(0, max_disp_x))(im_features_r_warped)
+    # new size is w+2*d
+    max_padded_disp_x = 2*max_disp_x+1
+
+    # iterate -d to +d
+    for disp_x in range(max_padded_disp_x):
+        im_r_shifted = tf.slice(im_features_r_padded, [
+                                0, 0, disp_x, 0], tf.shape(im_features_l))
         corr_slice = tf.reduce_mean(tf.multiply(
             im_features_l, im_r_shifted), axis=-1)
         corr_slices.append(corr_slice)
@@ -108,55 +114,75 @@ def correlation_volume(im_features_l, im_features_r_warped, max_disp_x=50):
 #         return pred_flow
 
 
-def corr_to_disp(corr_vol, num_conv_reps=5):
+def predict_disp(ft, num_conv_reps=5):
     # reduce channels from dispartiy to 1 by aplying convolutions repeatedly
     res_channels = [128, 96, 64, 32, 1]
     assert(len(res_channels) == num_conv_reps)
-    conv_corr_vol = corr_vol
-    for i in range(num_conv_reps):
-        conv_corr_vol = tf.keras.layers.Concatenate(axis=3)(
-            [conv_corr_vol, conv(res_channels[i])(conv_corr_vol)])
+    conv_ft = ft
+    for i in range(num_conv_reps-1):
+        # we donot use dense connections as of now
+        conv_ft = conv(res_channels[i])(conv_ft)
+    # original paper does not applies relu on last layer as they want both positive and negative flows but we want only
+    # positive disparities so relu is fine.
     # last layer is 1 channel, hence disparity
-    pred_disp = conv(res_channels[-1])(conv_corr_vol)
-    return pred_disp
+    pred_disp = conv(res_channels[-1])(conv_ft)
+    # last 32 dim ft. can be passed as upsampled features
+    return pred_disp, conv_ft
 
 
 def warp(im_features_r, disp_r_to_l__x):
     # disparity is positive
     # 0->y, 1->x
-    disp_y = tf.zeros(tf.shape(disp_r_to_l__x), name='zero_disp_y')
-    flow_r_to_l = tf.concat([disp_y, disp_r_to_l__x], axis=3)
+    disp_y = tf.zeros_like(disp_r_to_l__x, name='zero_disp_y')
+    flow_r_to_l = tf.concat([disp_y, disp_r_to_l__x], axis=-1)
     im_rec_l = tfa.image.dense_image_warp(im_features_r, flow_r_to_l)
     return im_rec_l
 
 
-def extract_multiscale_disps(img_features, num_scale_levels):
+def extract_multiscale_disps(img_features, num_levels, predict_level):
     LEFT, RIGHT = (0, 1)
-    disps = [None]*num_scale_levels
+    disps = [None]*num_levels
     warp_layer = tf.keras.layers.Lambda(lambda x: warp(x[0], x[1]))
-    for level in range(num_scale_levels-1, -1, -1):
-        if level+1 >= num_scale_levels:
-            disp_up = tf.zeros(
-                tf.concat([tf.shape(img_features[RIGHT][level])[:-1], tf.constant([1], dtype=tf.int32)], axis=0), name='no_flow')
+    for level in range(num_levels-1, predict_level-1, -1):
+        im_l_fts = img_features[LEFT][level]
+        im_r_fts = img_features[RIGHT][level]
+
+        if level == num_levels-1:
+            corr_vol = correlation_volume(im_l_fts, im_r_fts)
+            corr_vol = tf.keras.layers.LeakyReLU(0.1)(corr_vol)
+            corr_vol_enriched = tf.keras.layers.Concatenate(axis=3)(
+                [corr_vol, im_l_fts])
         else:
+            # upsample previous level flows and features
             disp_up = deconv(1, name='disp_upsample_{}'.format(
                 level+1))(disps[level+1])
+            res_feat_up = deconv(1, name='feat_upsample_{}'.format(
+                level+1))(res_feat)
 
-        # original paper scales gt flow by 20 but not in our case. hence, adjust accordingly
-        # flow is computed as original res disparity irrespective the level
-        upsampled_disp_scale_factor = 1./(2**(level+1+1))
-        img_r_warped = warp_layer(
-            inputs=[img_features[RIGHT][level], upsampled_disp_scale_factor*disp_up])
-        img_l_features = img_features[LEFT][level]
-        corr_vol = correlation_volume(img_l_features, img_r_warped)
-        corr_vol = tf.keras.layers.LeakyReLU(0.1)(corr_vol)
-        # todo: upsampled features convolved to 2 channels also concatenated in original paper
-        # left features are concatenated to create U-Net like architecture
-        corr_vol_enriched = tf.keras.layers.Concatenate(axis=3)(
-            [corr_vol, img_l_features, disp_up])
+            # original paper scales gt flow by 20 but not in our case. hence, adjust accordingly
+            # flow is computed as original res disparity irrespective the level
+            upsampled_disp_scale_factor = 1./2**level
+            im_r_warped = warp_layer(
+                inputs=[im_l_fts, upsampled_disp_scale_factor*disp_up])
+            corr_vol = correlation_volume(im_r_fts, im_r_warped)
+            corr_vol = tf.keras.layers.LeakyReLU(0.1)(corr_vol)
+            # left features are concatenated to create U-Net like architecture
+            # disparity is added from low resolution to upscale resolution, hence final disparity will be far greater than max_disp at each level
+            corr_vol_enriched = tf.keras.layers.Concatenate(axis=3)(
+                [disp_up, corr_vol, im_l_fts, res_feat_up])
 
-        disps[level] = corr_to_disp(corr_vol_enriched)
+        disps[level], res_feat = predict_disp(corr_vol_enriched)
     return disps
+
+
+def spatial_refine_flow(flow):
+    channels = [128, 128, 128, 96, 64, 32, 1]
+    dil_rates = [1, 2, 4, 8, 16, 1, 1]
+    assert(len(channels) == len(dil_rates))
+    for channel, dil_rate in zip(channels, dil_rates):
+        refined_flow = tf.keras.layers.Conv2D(
+            channel, 3, padding='same', dilation_rate=dil_rate)(flow)
+    return refined_flow
 
 
 def pyramid_l1_loss(flows_pred, flow_gt):
@@ -175,9 +201,9 @@ def pyramid_l1_loss(flows_pred, flow_gt):
 
 def masked_loss(disparity_true, disparity_pred):
     # gt disparity is sparse, compute loss only on pixels where we have disparity info
-    abs_error = tf.keras.backend.abs(
-        tf.subtract(disparity_pred, tf.cast(disparity_true, tf.float32)))
-    mask = disparity_true > 0
+    abs_error = tf.math.abs(
+        tf.math.subtract(disparity_pred, tf.cast(disparity_true, tf.float32)))
+    mask = disparity_true >= 1
     mae = tf.reduce_mean(tf.boolean_mask(abs_error, mask))
     return mae
 
@@ -198,30 +224,35 @@ def disparity_accuracy(disparity_true, disparity_pred):
 def build_model(img_shape):
     views = [{'id': 0, 'name': 'left_view'}, {'id': 1, 'name': 'right_view'}]
     LEFT, RIGHT = (0, 1)
-    num_feature_scale_levels = 6
-
+    num_pyramid_levels = 8
+    # 0-indexed, level at which prediction will happen
+    predict_level = 2
     preprocess_image, processed_img_shape = build_preprocess_image(
-        img_shape, num_feature_scale_levels)
-    extract_features_multiscale = build_pyramid_feature_extractor(
-        processed_img_shape, num_feature_scale_levels)
+        img_shape, num_pyramid_levels)
+    extract_ft_pyramid = build_pyramid_feature_extractor(
+        processed_img_shape, num_pyramid_levels)
 
     imgs = []
     ims_ft_pyramid = []
 
     for view in views:
-        im = build_image_input(img_shape, name=view['name'])
+        im = get_image_input(img_shape, name='{}_image'.format(view['name']))
         imgs.append(im)
         im_normed = preprocess_image(im)
-        im_ft_pyramid = extract_features_multiscale(im_normed)
+        im_ft_pyramid = extract_ft_pyramid(im_normed)
         ims_ft_pyramid.append(im_ft_pyramid)
 
     flow_pyramid = extract_multiscale_disps(
-        ims_ft_pyramid, num_feature_scale_levels)
+        ims_ft_pyramid, num_pyramid_levels, predict_level)
 
-    # original resolution
-    output_flow = 0.5*deconv(1, name='bilinear_upsample')(flow_pyramid[0])
+    smooth_flow = flow_pyramid[predict_level] + \
+        spatial_refine_flow(flow_pyramid[predict_level])
 
-    output_flow = crop(img_shape, processed_img_shape)(output_flow)
+    # original resolution bilinear upsample
+    h, w, _ = processed_img_shape
+    l0_flow = tf.image.resize(smooth_flow, (h, w))
+
+    output_flow = crop(l0_flow, img_shape)
     # build loss from these flows
     model = tf.keras.Model(inputs={
                            'left_view': imgs[0], 'right_view': imgs[1]}, outputs=output_flow, name='PWC_net')
